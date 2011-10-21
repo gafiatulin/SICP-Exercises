ee9b2915f34aa2a3460d8cdea82e251cb483581f
Victor Gafiatulin <gafiatulin@gmail.com>
2011-10-21 20:41:04 +0400

    Cube root function, same principle as sqrt.

---
+++ ex 1_6.clj
@@ -0,0 +1,8 @@
+(defn abs [x] (cond (< x 0) (- x) (> x 0) x :else 0))
+(defn cube [x] (* x x x))
+(defn square [x] (* x x))
+(defn average [x y] (/ (+ x y) 2))
+(defn improve [guess x] (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+(defn good-enough [guess x] (< (abs(- (cube guess) x)) 0.00001))
+(defn sbrt-itter [guess x] (if(good-enough guess x) guess (sbrt-itter (improve guess x) x)))
+(defn cbrt [x] (sbrt-itter 1.0 x))

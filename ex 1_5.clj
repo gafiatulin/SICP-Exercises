69aa6f9f9dbfe4c3d682997805f849760e1b0f9b
Victor Gafiatulin <gafiatulin@gmail.com>
2011-10-21 20:15:12 +0400

    sqrt function implemented.

---
+++ ex 1_5.clj
@@ -0,0 +1,11 @@
+(defn abs [x] (cond (< x 0) (- x) (> x 0) x :else 0))
+(defn square [x] (* x x))
+(defn average [x y] (/ (+ x y) 2))
+(defn good-enough [guess x] (< (abs(- (square guess) x)) 0.0001))
+(defn improve [guess x] (average guess (/ x guess)))
+(defn sqrt-iter [guess x]
+	(if(good-enough guess x) 
+		guess
+		(sqrt-iter (improve guess x) x))
+)
+(defn sqrt [x] (sqrt-iter 1.0 x))

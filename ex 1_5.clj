6d456b7dd63958447923e51696f1ce4d901cf4dd
Victor Gafiatulin <gafiatulin@gmail.com>
2011-10-21 20:19:27 +0400

    Nothing special

--- ex 1_5.clj
+++ ex 1_5.clj
@@ -3,9 +3,5 @@
 (defn average [x y] (/ (+ x y) 2))
 (defn good-enough [guess x] (< (abs(- (square guess) x)) 0.0001))
 (defn improve [guess x] (average guess (/ x guess)))
-(defn sqrt-iter [guess x]
-	(if(good-enough guess x) 
-		guess
-		(sqrt-iter (improve guess x) x))
-)
+(defn sqrt-iter [guess x] (if(good-enough guess x) guess (sqrt-iter (improve guess x) x)))
 (defn sqrt [x] (sqrt-iter 1.0 x))

f5b4d53fe49b20b52304621d7921cb9b5fc0943c
Victor Gafiatulin <gafiatulin@gmail.com>
2011-10-22 17:13:15 +0400

    Recursive & iterative version merged.

--- 1/factorial.clj
+++ 1/factorial.clj
@@ -1,6 +1,27 @@
-(defn factorial [n] 
+(defn factorial-rec [n] 
+	(if (= n 1) 
+		1 
+		(* 
+			n 
+			(factorial-rec 
+				(- n 1)
+			)
+		)
+	)
+)
+
+(defn factorial-it [n] 
 	(defn iter [product counter]
-		(if (> counter n) product  (iter (* counter product) (+ counter 1)))
+		(if (> counter n) 
+			product  
+			(iter 
+				(* counter product) 
+				(+ counter 1)
+			)
+		)
 	)
 	(iter 1 1)
 )
+
+(println (time (factorial-rec 10)))
+(println (time (factorial-it 10)))

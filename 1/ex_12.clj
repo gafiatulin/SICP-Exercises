5e5bdc51841b62222e97fb966c764837b60653b3
Victor Gafiatulin <gafiatulin@gmail.com>
2011-10-22 16:09:04 +0400

    Fancy formatting & printing.

--- 1/ex_12.clj
+++ 1/ex_12.clj
@@ -1,1 +1,36 @@
-(defn pas-tri [i j] (cond (or (> j i) (or (< j 1) (< i 1))) "Invalid" (or (= j 1) (= j i)) 1 :else (+ (pas-tri (- i 1) (- j 1)) (pas-tri (- i 1) j))))
+(defn binomial-coefficient [i j] 
+	(cond 
+		(or (> j i) (or (< j 1) (< i 1)))	"Invalid" 
+		(or (= j 1) (= j i))				1 
+		:else
+											(+ 
+												(binomial-coefficient (- i 1) (- j 1)) 
+												(binomial-coefficient (- i 1) j)
+											)
+	)
+)
+
+(defn pas-triangle-line [n] 
+	(for [x (range 1 (+ n 1))]  
+		(binomial-coefficient n x)
+	)
+)
+
+(defn pas-triangle [n] 
+	(for [x (range 1 (+ n 1))] 
+		(pas-triangle-line x)
+	)
+)
+
+(defn print-pas-triangle [n] 
+	(doseq [x (range 1 (+ n 1))] 
+		(doseq [y (range 1 (+ x 1))] 
+			( if (not= x y) 
+				(printf "%s " (binomial-coefficient x y)) 
+				(printf "%s\n" (binomial-coefficient x y))
+			)
+		)
+	)
+)
+
+(print-pas-triangle 10)

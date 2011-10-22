1f33ce0c2949dd5f8c16ceadb99bc1b082ca71f3
Victor Gafiatulin <gafiatulin@gmail.com>
2011-10-22 16:28:21 +0400

    Printing & fancy formatting added.

--- 1/ex_3.clj
+++ 1/ex_3.clj
@@ -1,2 +1,12 @@
-(defn square [x] (* x x))
-(defn myFn [x y z] (+ (square (max x y))( square (max (min x y) z))))
+(defn square [x] 
+	(* x x)
+)
+
+(defn myFn [x y z] 
+	(+ 
+		(square (max x y))
+		(square (max (min x y) z))
+	)
+)
+
+(print (myFn 3 4 5))

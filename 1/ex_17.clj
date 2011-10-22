07133063c9b72f45fc918240d890d86df8d8bbe1
Victor Gafiatulin <gafiatulin@gmail.com>
2011-10-22 20:30:18 +0400

    Function name changed.

--- 1/ex_17.clj
+++ 1/ex_17.clj
@@ -12,15 +12,15 @@
 	)
 )
 
-(defn fast-mul [x y]
+(defn fast-mul-rec [x y]
 	(cond 
 		(or (= x 0) (= y 0)) 0
 		(= y 1) x
-		(even? y) (dbl(fast-mul x (hlv y)))
-		:else (+ x (fast-mul x (- y 1)))
+		(even? y) (dbl(fast-mul-rec x (hlv y)))
+		:else (+ x (fast-mul-rec x (- y 1)))
 	)
 )
 
 (println (time (* 232434 5319)))
-(println (time(fast-mul 232434 5319)))
+(println (time(fast-mul-rec 232434 5319)))
 (println (time(mul 232434 5319)))

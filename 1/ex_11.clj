1be3702ac21ee3f816210e8abbaa6f4ef7a8f4e1
Victor Gafiatulin <gafiatulin@gmail.com>
2011-10-22 16:06:41 +0400

    Code formatting fix.

--- 1/ex_11.clj
+++ 1/ex_11.clj
@@ -1,6 +1,25 @@
-(def natural-numbers (iterate inc 1))
-(defn gen [x] (take x natural-numbers))
-(defn f-rec [n] (if (< n 3) n (+ (f-rec (- n 1)) (f-rec (- n 2)) (f-rec (- n 3)))))
-(defn f-it [n] (defn f-iter [a b c it] (cond (= it 1) c (= it 2) b (= it 3) a (> it 3) (f-iter (+ a b c) a b (- it 1)))) (f-iter 3 2 1 n))
+(defn f-rec [n] 
+	(if (< n 3) 
+		n 
+		(+ 
+			(f-rec (- n 1)) 
+			(f-rec (- n 2)) 
+			(f-rec (- n 3))
+		)
+	)
+)
+
+(defn f-it [n] 
+	(defn f-iter [a b c it] 
+		(cond 
+			(= it 1) c 
+			(= it 2) b 
+			(= it 3) a 
+			(> it 3) (f-iter (+ a b c) a b (- it 1))
+		)
+	) 
+	(f-iter 3 2 1 n)
+)
+
 (println (time (f-it 10)))
 (println (time  (f-rec 10)))
